4a3f7562c850eca9f421daf5215c1d98d12f2164
Kimyongjin <yongjin5184@gmail.com>
2020-12-02 12:55:58 +0900

    adapters > persistence 에 member entity 추가

---
+++ adapters/persistence/adapter-maria/src/main/java/domain/Member.java
@@ -0,0 +1,28 @@
+package domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private int authCode;
+    private String email;
+    private String name;
+    private String birthDay;
+    private String gender;
+    private String phoneNumber;
+    private boolean isAgreeMarketSns;
+}
